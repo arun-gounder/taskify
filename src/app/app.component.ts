73ac7b86360cacb91617d42a6ee5cf666c594e4d
Arun Gounder <gounderarun2812@gmail.com>
2024-03-03 18:56:56 +0530

    co,ponents added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { SidebarComponent } from './components/layouts/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'taskify';
