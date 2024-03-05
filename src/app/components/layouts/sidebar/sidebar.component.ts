7e7f1aba455116d9cb4397394c0d202e94f8012a
Arun Gounder <gounderarun2812@gmail.com>
2024-03-05 12:58:51 +0530

    sidebar added

--- src/app/components/layouts/sidebar/sidebar.component.ts
+++ src/app/components/layouts/sidebar/sidebar.component.ts
@@ -1,12 +1,33 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgFor, NgIf],
   templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent {
+  is_sidebar = true;
+  menu_items = [
+    {
+      title: 'Planning',
+      item: [
+        { content: 'Board', icon: "bx bx-time-five" }
+      ],
+      content: 'Board', icon: "bx bx-time-five"
+    },
+    { title: 'Developement', content: 'Code', icon: "bx bx-code-alt" },
+    { title: 'Operations', content: 'Developments', icon: "bx bx-briefcase" },
+  ];
+  expanded_index: number | null = null;
 
+  toggleItem(index: number): void {
+    if (this.expanded_index === index) {
+      this.expanded_index = null;
+    } else {
+      this.expanded_index = index;
+    }
+  }
 }
