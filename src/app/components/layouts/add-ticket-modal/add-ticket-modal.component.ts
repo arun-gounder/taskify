ffd75a6ea8a41a0bafa9992f6221a39883ce5150
Arun Gounder <gounderarun2812@gmail.com>
2024-03-06 10:17:09 +0530

    angular material added

---
+++ src/app/components/layouts/add-ticket-modal/add-ticket-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-ticket-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-ticket-modal.component.html',
+  styleUrl: './add-ticket-modal.component.css'
+})
+export class AddTicketModalComponent {
+
+}
--- src/app/pages/boards-page/boards-page.component.ts
+++ src/app/pages/boards-page/boards-page.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
-import { BoardCardComponent } from '../../components/layouts/board-card/board-card.component';
-
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+/**
+ * @title Drag&Drop connected sorting
+ */
 @Component({
   selector: 'app-boards-page',
+  templateUrl: './boards-page.component.html',
+  styleUrl: './boards-page.component.css',
   standalone: true,
-  imports: [BoardCardComponent],
-  templateUrl: './boards-page.component.html'
+  imports: [CdkDropList, CdkDrag, MatToolbarModule, MatButtonModule, MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule],
 })
 export class BoardsPageComponent {
 
+  // board_lists = {
+  //   todo: ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep', 'Arun'],
+  //   done: ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'],
+  // }
+  
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep', 'Arun'];
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
